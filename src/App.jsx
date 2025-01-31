d342d36f19df2926a7cbbae8d3ef08e2118bd864
Antonio Gagliardo <ajgagliardo92@gmail.com>
2025-01-30 23:04:58 -0800

    added some front end components like checkout and added a router tot he front end part to be able to include the backend soon

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import Characteristics from "./components/Characteristics";
 import Contact from "./components/Contact";
 import HeroSection from "./components/HeroSection";
+import Homepage from "./components/Homepage";
 import LastSubscribeSection from "./components/LastSubscribeSection";
 import Nav from "./components/Nav";
 import PriceSection from "./components/PriceSection";
 import Reviews from "./components/Reviews";
 import Social from "./components/Social";
+import CheckoutPage from "./components/CheckoutPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/checkout",
+    element: <CheckoutPage />,
+  },
+]);
 
 function App() {
   return (
     <>
-      <Nav />
-      <HeroSection />
-      <div className="sm:p-10 p-2 grid items-center justify-center box-border w-full">
-        <Characteristics className="" />
-
-        <PriceSection />
-        <Reviews />
-
-        <LastSubscribeSection />
-        {/* <Social /> */}
-        <Contact />
-      </div>
+      <RouterProvider router={router} />
+      {/* <Homepage /> */}
     </>
   );
 }
---
+++ src/components/CheckoutPage.jsx
@@ -0,0 +1,9 @@
+function CheckoutPage() {
+  return (
+    <div>
+      <h2>Checkout</h2>
+    </div>
+  );
+}
+
+export default CheckoutPage;
