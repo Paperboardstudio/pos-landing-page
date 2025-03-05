eac9f8182cb24ff99d5c14f6cbb409adf333bba2
Antonio Gagliardo <ajgagliardo92@gmail.com>
2025-03-04 20:42:24 -0800

    made changes on the custom checkout and shipping address component

--- src/components/checkout/CheckoutPage.jsx
+++ src/components/checkout/CheckoutPage.jsx
@@ -1,13 +1,30 @@
 import { Elements } from "@stripe/react-stripe-js";
 import StripeCheckout from "./StripeCheckout";
+import { CartContext } from "../context/cart-context";
 import { loadStripe } from "@stripe/stripe-js";
+import ShippingAddress from "./custom-checkout/Shipping-address";
+import { useContext, useState } from "react";
+import CustomCheckout from "./custom-checkout/custom-checkout";
 
 const stripePromise = loadStripe(import.meta.env.VITE_PUBLISHABLE_KEY);
 
 function CheckoutPage() {
+  const { itemCount, total, cartItems } = useContext(CartContext);
+  const [shipping, setShipping] = useState(null);
+  const addressShown = {
+    display: shipping ? "none" : "block",
+  };
+  const cardShown = { display: shipping ? "block" : "none" };
+
   return (
     <Elements stripe={stripePromise}>
-      <StripeCheckout />
+      {/* <StripeCheckout /> */}
+      <div style={addressShown}>
+        <ShippingAddress setShipping={setShipping} />
+      </div>
+      <div style={cardShown}>
+        <CustomCheckout {...{ shipping, cartItems }} />
+      </div>
     </Elements>
   );
 }
